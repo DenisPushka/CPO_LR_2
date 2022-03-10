7d2c0a732fb2db7a6b1afb8c64dd0fd9c51a0f37
Denis <bardenbmw@gmail.com>
2022-03-10 13:52:15 +0400

    вроде доделал

--- main.cpp
+++ main.cpp
@@ -12,64 +12,69 @@
 #pragma comment(lib, "pdh.lib")
 using namespace std;
 
-void moduleExp1();
+void moduleEntry(int p);
 
 void printProcess();
 
 void printOneProcess(int p);
 
-void PROCESS_MEMORY();
-
-double timeProcess(int process) {
-    FILETIME createTime;
-    FILETIME exitTime;
-    FILETIME kernelTime;
-    FILETIME userTime;
-    auto hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, process);
-    if (GetProcessTimes(hSnap,
-                        &createTime, &exitTime, &kernelTime, &userTime) != -1) {
-        SYSTEMTIME userSystemTime;
-        if (FileTimeToSystemTime(&userTime, &userSystemTime) != -1)
-            return (double) userSystemTime.wHour * 3600.0 +
-                   (double) userSystemTime.wMinute * 60.0 +
-                   (double) userSystemTime.wSecond +
-                   (double) userSystemTime.wMilliseconds / 1000.0;
-    }
-}
+void timeProcess(int p);
 
 int _tmain(int argc, _TCHAR *argv[]) {
     system("chcp 65001");
+
     printProcess();
-    moduleExp1();
-    /*printProcess();
     int process;
     cout << "\nВведите процесс: ";
     cin >> process;
     printOneProcess(process);
-    cout << "Время работы в редиме ядра: " << timeProcess(process);
-    auto hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, process);
+    timeProcess(process);
+    moduleEntry(process);
 
-    //PROCESS_MEMORY_COUNTERS Pr;
-    PROCESS_MEMORY_COUNTERS *Prq;
-    DWORD cbSize;
-    if (GetProcessMemoryInfo(hSnap, Prq, cbSize))
-        cout << "\n\nКол-во страничных ошибок: " << Prq->PageFaultCount;
-    return 0;*/
+    HANDLE hProcess = CreateToolhelp32Snapshot(TH32CS_SNAPALL, process);
+    PROCESS_MEMORY_COUNTERS memCounter;
+    if (GetProcessMemoryInfo(hProcess, &memCounter, sizeof(memCounter)))
+        cout << "\n\n\tКол-во страничных ошибок: " << memCounter.PageFaultCount;
+    else
+        cout << "-";
+
+    return 0;
 }
 
-void PROCESS_MEMORY(int a) {
-    cout << "\nВведите прикладной процесс: ";
-    auto hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, a);
-    PROCESSENTRY32 Pe;
-    Pe.dwSize = sizeof(Pe);
-    if (Process32First(hSnap, &Pe)) {
+void timeProcess(int process) {
+    auto hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, process);
+
+    PROCESSENTRY32 P;
+    P.dwSize = sizeof(PROCESSENTRY32);
+    if (Process32First(hSnap, &P)) {
         do {
-            if (a == Pe.th32ProcessID) {
-                cout << "Процесс: " << Pe.th32ProcessID << " \tВремя работы в режиме ядра: " << Pe.pcPriClassBase
-                     << "\tВремя работы в режиме пользователя: " << Pe.cntThreads << endl << endl;
+            if (P.th32ProcessID == process) {
+                HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, P.th32ProcessID);
+                FILETIME ft[4];
+                GetProcessTimes(hProcess, &ft[0], &ft[1], &ft[2], &ft[3]);
+                SYSTEMTIME smPR, smUs;
+                FileTimeToSystemTime(&ft[2], &smPR);
+                FileTimeToSystemTime(&ft[3], &smUs);
+                cout << "\n\tВремя в режиме ядра: " << smPR.wMilliseconds << "\tВ режиме пользователя: "
+                     << smUs.wMilliseconds;
                 break;
             }
-        } while (Process32Next(hSnap, &Pe));
+        } while (Process32Next(hSnap, &P));
+        CloseHandle(hSnap);
+    }
+}
+
+void moduleEntry(int process) {
+    auto hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, process);
+    MODULEENTRY32 Pe;
+    Pe.dwSize = sizeof(MODULEENTRY32);
+    if (Module32First(hSnap, &Pe)) {
+        do {
+            if (Pe.th32ProcessID == process) {
+                cout << "\n\tИмя модуля: " << Pe.szModule << "\tРазмер: " << Pe.dwSize << endl;
+                break;
+            }
+        } while (Module32Next(hSnap, &Pe));
         CloseHandle(hSnap);
     }
 }
@@ -81,29 +86,9 @@ void printOneProcess(int process) {
     if (Process32First(hSnap, &Pe)) {
         do {
             if (process == Pe.th32ProcessID) {
-                cout << "Процесс: " << Pe.th32ProcessID << " \tБазовый приоритет процессора: " << Pe.pcPriClassBase
-                     << "\t\tКол-во потоков: " << Pe.cntThreads << endl << endl;
+                cout << "\n\tПроцесс: " << Pe.th32ProcessID << " \tБазовый приоритет процессора: " << Pe.pcPriClassBase
+                     << "\tКол-во потоков: " << Pe.cntThreads << endl << endl;
                 break;
-            }
-        } while (Process32Next(hSnap, &Pe));
-        CloseHandle(hSnap);
-    }
-
-}
-
-void moduleExp1() {
-    int a;
-    cout << "Enter number process: ";
-    cin >> a;
-    auto hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, a);
-    PROCESSENTRY32 Pe;
-    Pe.dwSize = sizeof(PROCESSENTRY32);
-    if (Process32First(hSnap, &Pe)) {
-        do {
-            if (Pe.th32ParentProcessID == a) {
-                cout << Pe.th32ProcessID << " " <<  Pe.th32ParentProcessID << endl;
-                wprintf(L"%1s %-30s \n", " ", Pe.szExeFile);
-                // break;
             }
         } while (Process32Next(hSnap, &Pe));
         CloseHandle(hSnap);
